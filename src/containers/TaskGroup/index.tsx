e1acbf510c58f0a733f8b8236378b1a152565baa
Devin Eldreth <devin.eldreth@gmail.com>
2018-05-08 15:05:19 -0400

    Remove withAggregate and withGroups in favor of withProps composition

--- src/containers/TaskGroup/index.tsx
+++ src/containers/TaskGroup/index.tsx
@@ -3,14 +3,12 @@ import React from 'react';
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
 import { RouteComponentProps, withRouter } from 'react-router-dom';
-import { compose, withHandlers } from 'recompose';
+import { compose, withHandlers, withProps } from 'recompose';
 import styled from 'styled-components';
 
 import { ALL_TASKS_QUERY, GROUP_TASKS_QUERY } from 'app/graph/queries';
 import withQuery from 'app/graph/withQuery';
 
-import withAggregates, { Aggregate } from 'app/containers/TaskGroup/withAggregates';
-import withGroups from 'app/containers/TaskGroup/withGroups';
 import { Icon, ListContainer, ListItem, Loading } from 'app/styles';
 import { Task } from 'app/types';
 
@@ -20,7 +18,12 @@ export interface InjectedProps {
   allTasks: Task[];
   loading: boolean;
   routeTo: ( path: string ) => () => void;
-  aggregates: Aggregate;
+  aggregates: {
+    [ group: string ]: {
+      completed: number,
+      total: number,
+    },
+  };
   groups: string[];
 }
 
@@ -61,8 +64,40 @@ function TaskGroup ( { allTasks, loading, routeTo, aggregates, groups }: Props )
 export default compose<InjectedProps, ExternalProps>(
   withRouter,
   withQuery( ALL_TASKS_QUERY ),
-  withGroups,
-  withAggregates,
+  withProps( ( { allTasks, loading }: Props ) => {
+    if ( loading ) {
+      return {};
+    }
+
+    const groups: string[] = [];
+    allTasks.forEach( task => {
+      if ( !groups.includes( task.group ) ) {
+        groups.push( task.group );
+      }
+    } );
+
+    return { groups };
+  } ),
+  withProps( ( { allTasks, loading }: Props ) => {
+    if ( loading ) {
+      return {};
+    }
+
+    const aggregates: Props['aggregates'] = {};
+    allTasks.forEach( task => {    
+      if ( !aggregates.hasOwnProperty( task.group ) ) {
+        aggregates[ task.group ] = {
+          completed: 0,
+          total: 0,
+        };
+      }
+      
+      aggregates[ task.group ].completed += task.completedAt ? 1 : 0;
+      aggregates[ task.group ].total += 1;
+    } );
+
+    return { aggregates };
+  } ),
   withHandlers( {
     routeTo: ( { history }: Props ) => ( value: string )  => () => {
       history.push( value );
