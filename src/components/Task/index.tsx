7783a4596b07eda71fa02a11ce6338c26b0777d3
Devin Eldreth <devin.eldreth@gmail.com>
2018-05-23 09:32:05 -0400

    Cleanup the Task component
    * Move extraneous functions into helper file

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -4,6 +4,7 @@ import gql from 'graphql-tag';
 import { DateTime } from 'luxon';
 import { Query } from 'react-apollo';
 import compose from 'recompose/compose';
+import withHandlers from 'recompose/withHandlers';
 import styled from 'styled-components';
 
 import { UPDATE_TASK_MUTATION } from 'app/graph/mutations';
@@ -13,6 +14,7 @@ import withQuery from 'app/graph/withQuery';
 
 import { Icon, ListItem, Loading } from 'app/styles';
 import { Task as TaskType } from 'app/types';
+import { resolveLocks, resolveParentLocks } from './helpers';
 
 interface ExternalProps {
   task: TaskType;
@@ -22,11 +24,24 @@ export interface InjectedProps {
   allTasks: TaskType[];
   loading: boolean;
   updateTask: ( id: any ) => void;
+  onClick: ( locked: boolean ) => void;
 }
 
-type Props = ExternalProps & InjectedProps;
+export type Props = ExternalProps & InjectedProps;
 
-function Task ( { task, allTasks, loading, updateTask }: Props ) {
+const renderIcon = function renderTaskIcon( task: TaskType, taskIsLocked: boolean ) {
+  if ( taskIsLocked ) {
+    return <img src={ require( 'static/Locked.svg' ) }/>;
+  }
+
+  if ( task.completedAt ) {
+    return <img src={ require( 'static/Completed.svg' ) }/>;
+  } else {
+    return <img src={ require( 'static/Incomplete.svg' ) }/>;
+  }
+};
+
+const TaskComponent = function Task ( { task, allTasks, loading, updateTask, onClick }: Props ) {
   if ( loading ) {
     return <Loading>...</Loading>;
   }
@@ -37,75 +52,31 @@ function Task ( { task, allTasks, loading, updateTask }: Props ) {
     <TaskItem
       locked={ locked }
       completed={ task.completedAt !== null }
-      onClick={ () => {
-        if ( !locked ) {
-          updateTask( { variables: { 
-            id: task.id,
-            completedAt: task.completedAt ? null : DateTime.local().toISO(),
-          } } );
-
-          resolveParentLocks( task.id, allTasks, updateTask );
-        }
-      } }>
-      <Icon>{ renderTaskIcon( task, locked ) }</Icon>
+      onClick={ onClick( locked ) }>
+      <Icon>{ renderIcon( task, locked ) }</Icon>
       { task.task }
     </TaskItem>
   );
-}
-
-/**
- * resolveParentLocks - Iterate over all tasks and determine
- * parent relationship. If a parent is being placed in a locked state
- * then the task needs to be updated to reflect that.
- * 
- * This probably isn't the most performant approach... maybe offload
- * batch writes to cache?
- */
-function resolveParentLocks ( id: string,
-                              allTasks: TaskType[],
-                              updateTask: Props['updateTask'] ) {
-  const depends: string[] = [];
-  // Recurse the dependency graph
-  const parentIds = ( treeId: string ) => {
-    allTasks.forEach( task => {
-      if ( task.dependencyIds.includes( parseInt( treeId, 10 ) ) && !depends.includes( task.id ) && task.completedAt ) {
-        depends.push( task.id );
-        updateTask( { variables: { id: task.id, completedAt: null } } );
-        return parentIds( task.id );
-      }
-    } );
-  };
-  
-  parentIds( id );
-}
-
-function resolveLocks ( dependencies: number[], allTasks: TaskType[] ): boolean {
-  let locked = false;
-  allTasks.forEach( depends => {
-    if ( dependencies.includes( parseInt( depends.id, 10 ) ) && !depends.completedAt ) {
-      locked = true;
-    }
-  } );
-
-  return locked;
-}
-
-function renderTaskIcon ( task: TaskType, taskIsLocked: boolean ) {
-  if ( taskIsLocked ) {
-    return <img src={ require( 'static/Locked.svg' ) }/>;
-  }
-
-  if ( task.completedAt ) {
-    return <img src={ require( 'static/Completed.svg' ) }/>;
-  } else {
-    return <img src={ require( 'static/Incomplete.svg' ) }/>;
-  }
-}
+};
 
 export default compose<InjectedProps, ExternalProps>(
   withQuery( ALL_TASKS_QUERY ),
   withMutation( UPDATE_TASK_MUTATION, 'updateTask' ),
-)( Task );
+  withHandlers( {
+    onClick: ( { task, allTasks, updateTask }: Props ) =>
+                                   ( locked: boolean ) =>
+                                        ( event: any ) => {
+      if ( !locked ) {
+        updateTask( { variables: { 
+          id: task.id,
+          completedAt: task.completedAt ? null : DateTime.local().toISO(),
+        } } );
+
+        resolveParentLocks( task.id, allTasks, updateTask );
+      }
+    },
+  } ),
+)( TaskComponent );
 
 interface TaskItemProps {
   locked: boolean;
